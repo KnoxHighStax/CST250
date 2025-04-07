03e7fb81010ba9c099f44c1abe3db55f8dc2e666
KnoxHighStax <peytonwolf1234@gmail.com>
2025-04-07 12:55:59 -0400

    Add files via upload

---
+++ Activity1_CarStoreApplication/CarClassLibrary/FileIO.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CarClassLibrary
+{
+    public class FileIO
+    {
+        Store store;
+
+        // Constructor gets a reference to teh store, the inventory will be saved to the loaded from a file
+        public FileIO(Store store)
+        {
+            this.store = store;
+        }
+
+        // Store.txt file will be created in the same directory as the executable
+        public void SaveInventory()
+        {
+            string filename = "store.txt";
+
+            // It's a best practice to use "using" statements to ensure that IDisposable objects are disposed of correctly.
+            // Stream writer is used to write text to a file.
+            using (StreamWriter writer = new StreamWriter(filename))
+            {
+                // Write each car to the file. One car per line, with fields separated by commas.
+                foreach (Car car in store.CarList)
+                {
+                    writer.WriteLine($"{car.Make},{car.Model},{car.Year},{car.Price}");
+                }
+            }
+        }
+
+        // Load the store from the file. This method returns a list of cars.
+        public List<Car> LoadStore()
+        {
+            List<Car> cars = new List<Car>();
+            string filename = "store.txt";
+
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // Split the line into parts using the coma as a separator
+                    string[] parts = line.Split(',');
+                    string make = parts[0];
+                    string model = parts[1];
+                    // Always good to handle potential format issues with int and decimal parsing
+                    try
+                    {
+                        int year = int.Parse(parts[2]);
+                        decimal price = decimal.Parse(parts[3]);
+                        Car car = new Car(make, model, year, price);
+                        cars.Add(car);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error parsing data for car: " + line);
+                    }
+                }
+            }
+            return cars;
+        }
+
+    }
+}
---
+++ Activity1_CarStoreApplication/CarsShopConsoleApp/Program.cs
@@ -0,0 +1,77 @@
+﻿using CarClassLibrary;
+
+Store store = new Store();
+
+Console.Out.WriteLine("Welcome to the car shop! First you must create some cars and put them into the inventory. " +
+    "Then you may add cars to the cart. Finally, you may checkout, which will calculate your total bill.");
+
+int action = ChooseAction();
+
+while (action != 0)
+{
+    switch (action)
+    {
+        case 1:
+            Console.WriteLine("Enter the make of the car: ");
+            string make = Console.ReadLine();
+            Console.WriteLine("Enter the model of the car: ");
+            string model = Console.ReadLine();
+            Console.WriteLine("Enter the year of the car: ");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the price of the car: ");
+            decimal price = decimal.Parse(Console.ReadLine());
+            Car car = new Car(make, model, year, price);
+            store.CarList.Add(car);
+            break;
+        case 2:
+            PrintStoreInventory();
+            Console.WriteLine("Enter the index of the car you would like to add to the car: ");
+            int index = int.Parse(Console.ReadLine());
+            store.ShoppingList.Add(store.CarList[index]);
+            break;
+        case 3:
+            decimal total = store.Checkout();
+            PrintShoppingList();
+            Console.WriteLine("Your total is: " + total);
+            break;
+        case 4:
+            FileIO fileIO = new FileIO(store);
+            fileIO.SaveInventory();
+            break;
+        case 5:
+            FileIO fileIO2 = new FileIO(store);
+            store.CarList = fileIO2.LoadStore();
+            break;
+        default:
+            Console.WriteLine("Invalid choise");
+            break;
+    }
+    PrintStoreInventory();
+    action = ChooseAction();
+}
+
+int ChooseAction()
+{
+    int choice = 0;
+    Console.WriteLine("Choose an action: (0) Quit (1) Create Car (2) Add car to the cart (3) Checkout (4) Save inventory to a text file (5) Load inventory from text file.");
+    choice = int.Parse(Console.ReadLine());
+    return choice;
+}
+
+void PrintStoreInventory()
+{
+    Console.WriteLine("Inventory: ");
+    for (int i = 0; i < store.CarList.Count; i++)
+    {
+        Console.WriteLine(i + ": " + store.CarList[i]);
+    }
+}
+
+void PrintShoppingList()
+{
+    Console.WriteLine("Shopping List: ");
+    for (int i = 0; i < store.ShoppingList.Count; i++)
+    {
+        Console.WriteLine(i + ": " + store.ShoppingList[i]);
+    }
+}
