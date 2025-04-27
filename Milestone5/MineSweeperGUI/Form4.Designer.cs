7900b373db84e0771bb87912d9a73320c901d633
KnoxHighStax <peytonwolf1234@gmail.com>
2025-04-27 18:17:05 -0400

    Add files via upload

---
+++ Milestone5/MineSweeperGUI/Form4.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace MineSweeperGUI
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            loadToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            sortToolStripMenuItem = new ToolStripMenuItem();
+            byNameToolStripMenuItem = new ToolStripMenuItem();
+            byScoreToolStripMenuItem = new ToolStripMenuItem();
+            byDateToolStripMenuItem = new ToolStripMenuItem();
+            label1 = new Label();
+            dataGridViewHighScores = new DataGridView();
+            btnHSOK = new Button();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewHighScores).BeginInit();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, sortToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { saveToolStripMenuItem, loadToolStripMenuItem, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(46, 24);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.Size = new Size(224, 26);
+            saveToolStripMenuItem.Text = "Save";
+            saveToolStripMenuItem.Click += saveToolStripMenuItem_Click;
+            // 
+            // loadToolStripMenuItem
+            // 
+            loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            loadToolStripMenuItem.Size = new Size(224, 26);
+            loadToolStripMenuItem.Text = "Load";
+            loadToolStripMenuItem.Click += loadToolStripMenuItem_Click;
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(224, 26);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // sortToolStripMenuItem
+            // 
+            sortToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { byNameToolStripMenuItem, byScoreToolStripMenuItem, byDateToolStripMenuItem });
+            sortToolStripMenuItem.Name = "sortToolStripMenuItem";
+            sortToolStripMenuItem.Size = new Size(50, 24);
+            sortToolStripMenuItem.Text = "Sort";
+            // 
+            // byNameToolStripMenuItem
+            // 
+            byNameToolStripMenuItem.Name = "byNameToolStripMenuItem";
+            byNameToolStripMenuItem.Size = new Size(224, 26);
+            byNameToolStripMenuItem.Text = "By Name";
+            byNameToolStripMenuItem.Click += byNameToolStripMenuItem_Click;
+            // 
+            // byScoreToolStripMenuItem
+            // 
+            byScoreToolStripMenuItem.Name = "byScoreToolStripMenuItem";
+            byScoreToolStripMenuItem.Size = new Size(224, 26);
+            byScoreToolStripMenuItem.Text = "By Score";
+            byScoreToolStripMenuItem.Click += byScoreToolStripMenuItem_Click;
+            // 
+            // byDateToolStripMenuItem
+            // 
+            byDateToolStripMenuItem.Name = "byDateToolStripMenuItem";
+            byDateToolStripMenuItem.Size = new Size(224, 26);
+            byDateToolStripMenuItem.Text = "By Date";
+            byDateToolStripMenuItem.Click += byDateToolStripMenuItem_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(88, 20);
+            label1.TabIndex = 1;
+            label1.Text = "High Scores";
+            // 
+            // dataGridViewHighScores
+            // 
+            dataGridViewHighScores.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewHighScores.Location = new Point(12, 58);
+            dataGridViewHighScores.Name = "dataGridViewHighScores";
+            dataGridViewHighScores.RowHeadersWidth = 51;
+            dataGridViewHighScores.Size = new Size(561, 341);
+            dataGridViewHighScores.TabIndex = 2;
+            // 
+            // btnHSOK
+            // 
+            btnHSOK.Location = new Point(27, 409);
+            btnHSOK.Name = "btnHSOK";
+            btnHSOK.Size = new Size(94, 29);
+            btnHSOK.TabIndex = 3;
+            btnHSOK.Text = "OK";
+            btnHSOK.UseVisualStyleBackColor = true;
+            btnHSOK.Click += btnHSOK_Click;
+            // 
+            // Form4
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnHSOK);
+            Controls.Add(dataGridViewHighScores);
+            Controls.Add(label1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form4";
+            Text = "Form4";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewHighScores).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem sortToolStripMenuItem;
+        private Label label1;
+        private DataGridView dataGridViewHighScores;
+        private Button btnHSOK;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem loadToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem byNameToolStripMenuItem;
+        private ToolStripMenuItem byScoreToolStripMenuItem;
+        private ToolStripMenuItem byDateToolStripMenuItem;
+    }
+}
---
+++ Milestone5/MineSweeperGUI/Form4.cs
@@ -0,0 +1,176 @@
+﻿using MineSweeperClasses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineSweeperGUI
+{
+    public partial class Form4 : Form
+    {
+        // Stores current game stats
+        GameStat gameStat;
+        // Used for data binding with the DataGridView
+        BindingSource bindingSource = new BindingSource();
+        // List to store all game stats
+        public List<GameStat> statList = new List<GameStat>();
+
+        // Constructor taht intializes the high scores form with player name and score
+        public Form4(string name, int score)
+        {
+            InitializeComponent();
+            // Creating new game stat entry
+            gameStat = new GameStat();
+            gameStat.Name = name;
+            gameStat.Score = score;
+            gameStat.Date = DateTime.Now;
+            gameStat.Id = statList.Count + 1;
+            statList.Add(gameStat);
+
+            // Setup DataGridView
+            bindingSource.DataSource = typeof(GameStat);
+            bindingSource.DataSource = statList;
+            dataGridViewHighScores.DataSource = bindingSource;
+
+            // Confirm menu
+            ConfigureMenu();
+        }
+
+        // Method to set up the event handlers for the menu items
+        private void ConfigureMenu()
+        {
+            saveToolStripMenuItem.Click += saveToolStripMenuItem_Click;
+            loadToolStripMenuItem.Click += loadToolStripMenuItem_Click;
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            byScoreToolStripMenuItem.Click += byScoreToolStripMenuItem_Click;
+            byDateToolStripMenuItem.Click += byDateToolStripMenuItem_Click;
+        }
+
+        // Method to close the high scores form when OK button is clicked
+        private void btnHSOK_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Method that handles saving the high score to a file
+        private void saveToolStripMenuItem_Click(object? sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Creating a temporary file for safe saving
+                    string tempFile = Path.GetTempFileName();
+
+                    // To preserve existing file if is available
+                    if (File.Exists(saveFileDialog.FileName))
+                    {
+                        File.Copy(saveFileDialog.FileName, tempFile, true);
+                    }
+
+                    // Write all stats to the file
+                    using (StreamWriter writer = File.AppendText(tempFile))
+                    {
+                        foreach (var stat in statList)
+                        {
+                            writer.WriteLine($"{stat.Id},{stat.Name},{stat.Score},{stat.Date}");
+                        }
+                    }
+                    // To replace the original file with the temporary file
+                    File.Replace(tempFile, saveFileDialog.FileName, null);
+
+                    MessageBox.Show($"High scores appended successfully!", "Success",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error saving file: {ex.Message}", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // Method that handles loading the high scores from a file
+        private void loadToolStripMenuItem_Click(object? sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Clear the existing stats
+                    statList.Clear();
+                    // Read file line by line
+                    using (StreamReader reader = new StreamReader(openFileDialog.FileName))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            string[] parts = line.Split(',');
+                            if (parts.Length == 4)
+                            {
+                                // Create new GameStat from file data
+                                GameStat stat = new GameStat
+                                {
+                                    Id = int.Parse(parts[0]),
+                                    Name = parts[1],
+                                    Score = int.Parse(parts[2]),
+                                    Date = DateTime.Parse(parts[3]),
+                                };
+                                statList.Add(stat);
+                            }
+                        }
+                    }
+                    // Refresh the data binding
+                    bindingSource.ResetBindings(false);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error loading file: {ex.Message}", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // Method that will close the high scores form
+        private void exitToolStripMenuItem_Click(object? sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Method to sort score by name
+        private void byNameToolStripMenuItem_Click(object? sender, EventArgs e)
+        {
+            statList = statList.OrderBy(s => s.Name).ToList();
+            bindingSource.DataSource = null;
+            bindingSource.DataSource = statList;
+            dataGridViewHighScores.Refresh();
+        }
+
+        // Method that sorts scores by score
+        private void byScoreToolStripMenuItem_Click(object? sender, EventArgs e)
+        {
+            statList = statList.OrderByDescending(s => s.Score).ToList();
+            bindingSource.DataSource = null;
+            bindingSource.DataSource = statList;
+            dataGridViewHighScores.Refresh();
+        }
+
+        // Method that sorts scores by date
+        private void byDateToolStripMenuItem_Click(object? sender, EventArgs e)
+        {
+            statList = statList.OrderByDescending(s => s.Date).ToList();
+            bindingSource.DataSource = null;
+            bindingSource.DataSource = statList;
+            dataGridViewHighScores.Refresh();
+        }
+    }
+}
